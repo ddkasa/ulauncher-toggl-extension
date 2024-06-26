f055ee3cc35e606f38672271fa81412883c53b3d
David Kasakaitis <davidkasakaitis@proton.me>
2024-06-26 15:41:11 +0100

    chore: bump wrapper dependency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ulauncher_toggl_extension.utils import ensure_import
 
-ensure_import("toggl_api", "toggl-api-wrapper", "0.2.3")
+ensure_import("toggl_api", "toggl-api-wrapper", "0.3.0")
 
 from ulauncher_toggl_extension.extension import TogglExtension  # noqa: E402
 
