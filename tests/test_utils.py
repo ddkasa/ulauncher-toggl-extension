e8bcedf775ec75dcb53c523c1ceebce9f5e54351
David Kasakaitis <davidkasakaitis@proton.me>
2024-08-13 12:23:12 +0100

    refactor: remove uneccessary production files

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from ulauncher_toggl_extension.utils import ensure_import
-
-
-@pytest.mark.unit()
-def test_ensure_import():
-    with pytest.raises((ModuleNotFoundError,)):
-        ensure_import("wadwadwd", "wadwadawdw")
