d4c4a1af399627c22230a1d71dab98536ef5fa66
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-15 13:43:48 +0100

    chore(deps): update toggl api wrapper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ulauncher_toggl_extension.utils import ensure_import
 
-ensure_import("toggl_api", "toggl-api-wrapper", "0.3.1")
+ensure_import("toggl_api", "toggl-api-wrapper", "1.0.1")
 
 from ulauncher_toggl_extension.extension import TogglExtension  # noqa: E402
 
