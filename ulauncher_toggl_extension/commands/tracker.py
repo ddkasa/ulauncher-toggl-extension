00bcc31eff6955b2c026b75728f8ca990bda6bc1
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-30 17:15:23 +0000

    fix(trackers): stop command assigning wrong variable

--- ulauncher_toggl_extension/commands/tracker.py
+++ ulauncher_toggl_extension/commands/tracker.py
@@ -984,7 +984,7 @@ class DeleteCommand(TrackerCommand):
             current_cmd = CurrentTrackerCommand(self)
             current_tracker = current_cmd.get_current_tracker()
             if current_tracker and tracker.id == current_tracker.id:
-                current_cmd.current_tracker = None
+                current_cmd.tracker = None
             self.notification(msg=f"Removed {tracker.name}!")
             return True
 
