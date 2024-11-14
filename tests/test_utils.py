f02ed2d6f6bcd4c9c766de257bd4ae0c4c593866
David Kasakaitis <davidkasakaitis@proton.me>
2024-11-14 08:26:52 +0000

    test(utils): check if all versions match up

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
+import subprocess  # noqa: S404
 from pathlib import Path
 
 import pytest
-from pip._vendor import tomli  # noqa: PLC2701
+import tomli
 
 from ulauncher_toggl_extension import __version__
 from ulauncher_toggl_extension.utils import ensure_import
@@ -13,6 +14,15 @@ def test_version():
         pyversion = tomli.load(pyproject)["tool"]["poetry"]["version"]
 
     assert __version__ == pyversion
+
+    git_tag = subprocess.run(  # noqa: S603
+        ["git", "describe", "--abbrev=0", "--tags"],  # noqa: S607
+        stdout=subprocess.PIPE,
+        check=True,
+        text=True,
+    ).stdout.strip()[1:]
+
+    assert __version__ == git_tag
 
 
 @pytest.mark.unit
